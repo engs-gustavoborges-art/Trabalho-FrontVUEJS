5fcb22d3458504c10f4f8b4da6d8874abab0f202
Gustavo <engs-gustavoborges@camporeal.edu.br>
2026-06-11 12:18:47 -0300

    home.js

--- src/router/home.js
+++ src/router/home.js
@@ -33,6 +33,20 @@ export default {
         genero: 'Animacao',
         nota: 8.3,
         imagem: 'https://upload.wikimedia.org/wikipedia/pt/a/a7/Toy_Story_1995.jpg'
+      },
+      {
+        id: 9,
+        titulo: 'Homem-Aranha: Sem Volta Para Casa',
+        genero: 'Acao',
+        nota: 8.2,
+        imagem: 'https://upload.wikimedia.org/wikipedia/pt/0/00/Spider-Man_No_Way_Home_poster.jpg'
+      },
+      {
+        id: 10,
+        titulo: 'O Rei Leao',
+        genero: 'Aventura',
+        nota: 8.5,
+        imagem: 'https://static.wikia.nocookie.net/disney/images/c/c7/Poster-rei-leao-full.jpeg/revision/latest?cb=20160925204728&path-prefix=pt-br'
       }
     ])
 
@@ -64,6 +78,20 @@ export default {
         genero: 'Animacao',
         nota: 7.6,
         imagem: 'https://m.media-amazon.com/images/S/pv-target-images/aa61ae8fb015160d802c4d5cb4fe6858058ea76485c3498ed9ff431eee4fc83f.jpg'
+      },
+      {
+        id: 11,
+        titulo: 'Gladiador 2',
+        genero: 'Acao',
+        nota: 6.8,
+        imagem: 'https://m.media-amazon.com/images/M/MV5BN2E2NTI2NDUtY2Y0ZC00NDJmLTk1NmYtOGEzZTRlODYxMDNlXkEyXkFqcGc@._V1_.jpg'
+      },
+      {
+        id: 12,
+        titulo: 'Moana 2',
+        genero: 'Animacao',
+        nota: 6.7,
+        imagem: 'https://upload.wikimedia.org/wikipedia/pt/7/73/Moana_2_poster.jpg'
       }
     ])
 
